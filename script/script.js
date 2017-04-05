b870b681d8614d546131a176e1768c28daaf2ae9
Joey Robertson <JoeyLRobertson@gmail.com>
2017-04-05 14:21:10 -0500

    Draw and reset are functional, grid resize not functional

--- script/script.js
+++ script/script.js
@@ -1,10 +1,29 @@
-//var gridSize = prompt("what size grid would you like? Might I suggest 16?");
+var gridSize = 17;
+var cellSize = 700/gridSize;
 
 $(document).ready(function() {
-  for(var i=0; i < 16; i++) {
-    for(var j=0; j < 16; j++){
+  drawBoard();
+
+  $('.unit').hover(function(){
+    $(this).css("background-color", "black");
+  });
+
+  $('#reset').click(function(){
+    $('.unit').css("background-color", "white");
+    gridSize = prompt("what size grid would you like?");
+    cellSize = 700/gridSize;
+    drawBoard();
+  });
+
+});
+
+function drawBoard() {
+  for(var i=0; i < gridSize; i++) {
+    for(var j=0; j < gridSize; j++){
       var unit = $('<div class="unit"></div>');
+      $('.unit').height(cellSize);
+      $('.unit').width(cellSize);
       unit.appendTo('#wrapper');
     }
   }
-});
+}
